f037dd0db617e5aa9123f7618fe30bbe6a2d1b66
adityarathor007 <adityarathor120@gmail.com>
2024-04-21 23:35:24 +0530

    final touches

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -3,13 +3,12 @@ import React from 'react'
 function Navbar() {
   return (
     <div>
-       <p className='header_context'>Github: 
-       <a href="https://github.com/adityarathor007/Face_Recognition"> Repo</a>
-        </p>
-
+      <p className='header_context'>Github: 
+  <a href="https://github.com/adityarathor007/Face_Recognition" style={{ textDecoration: 'none' }}>Repository Link</a>
+</p>
          <p className='header_context'>Paper:
        
-          <a href=""> Paper </a>
+          <a href="" style={{ textDecoration: 'none' }}> Report </a>
         </p>
     </div>
   )
