ba77d712d9da0bf61702321a124c26e363b0cfa0
adityarathor007 <adityarathor120@gmail.com>
2024-04-21 23:18:24 +0530

    final report changes

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -4,12 +4,12 @@ function Navbar() {
   return (
     <div>
        <p className='header_context'>Github: 
-       <a href="https://github.com/adityarathor007/Face_Recognition"> Repo</a>.
+       <a href="https://github.com/adityarathor007/Face_Recognition"> Repo</a>
         </p>
 
-         <p className='header_context'>Paper: 
+         <p className='header_context'>Paper:
        
-          <a href=""> Paper </a>.
+          <a href=""> Paper </a>
         </p>
     </div>
   )
